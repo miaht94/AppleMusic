dcc4c43bd2c597df1f65a7bd8e0ebb4049468132
Trần Xuân Bách <tranxuanbach1@gmail.com>
2022-03-23 20:52:08 +0700

    Temporary done search bar without animation

---
+++ lib/components/SearchBar/SearchBar.dart
@@ -0,0 +1,143 @@
+import 'package:apple_music/components/SearchBar/SearchBarConstant.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_sfsymbols/flutter_sfsymbols.dart';
+
+class SearchBar extends StatefulWidget {
+    SearchBar({
+        Key ? key
+    }): super(key: key);
+
+    @override
+    State < SearchBar > createState() => _SearchBarState();
+}
+
+class _SearchBarState extends State < SearchBar > {
+    Color cancelButtonFontColor = kCancelButtonFontColor;
+    late TextEditingController inputController;
+    FocusNode focusNode = FocusNode(canRequestFocus: true);
+    bool xMarkVisible = false;
+    @override
+    void initState() {
+        // TODO: implement initState
+
+        super.initState();
+        inputController = TextEditingController(text: "");
+        inputController.addListener(() {
+            print(inputController.text);
+            if (inputController.text != "") {
+                setState(() {
+                    xMarkVisible = true;
+                });
+            } else {
+                setState(() {
+                    xMarkVisible = false;
+                });
+            }
+        }, );
+        focusNode.addListener(() {
+            if (focusNode.hasFocus) {
+
+            }
+        });
+
+    }
+    void onTapDownCancelBtn() {
+        setState(() {
+            cancelButtonFontColor = kCancelButtonFontColor.withOpacity(0.5);
+        });
+    }
+
+    void onTapUpCancelBtn() {
+        setState(() {
+            cancelButtonFontColor = kCancelButtonFontColor.withOpacity(1);
+            // inputController.text = "";
+
+            inputController.clear();
+            focusNode.unfocus();
+        });
+    }
+    @override
+    Widget build(BuildContext context) {
+        Size screenSize = MediaQuery.of(context).size;
+        return LayoutBuilder(builder: ((context, constraints) {
+            print(constraints);
+            return Row(
+                children: [
+                    Expanded(
+                        flex: 20,
+                        child: Container(
+                            //   width: focusNode.hasFocus ? constraints.maxWidth * kSearchBarWidthRatio : constraints.maxWidth,
+                            height: constraints.maxWidth * kSearchBarHeightRatio,
+                            alignment: Alignment.center,
+                            //   constraints: BoxConstraints(maxHeight: kSearchBarMaxHeight, minHeight: kSearchBarMinHeight, minWidth: kSearchBarMinWidth),
+                            decoration: BoxDecoration(
+                                color: kBackgroundSearchBarColor,
+                                borderRadius: BorderRadius.circular(kSearchBarBorderRadius)
+                            ),
+                            child: Row(
+                                // mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                    Icon(SFSymbols.search, color: kPlaceholderColor, ),
+                                    SizedBox(width: kPaddingBtwIconAndSearchBar, ),
+                                    Expanded(
+                                        child: TextField(
+                                            autocorrect: false,
+                                            focusNode: focusNode,
+                                            controller: inputController,
+                                            textAlignVertical: TextAlignVertical.center,
+                                            decoration: InputDecoration(
+                                                hintText: "Search ...",
+                                                border: InputBorder.none
+                                            ),
+                                        )
+                                    ),
+                                    
+                                    if (xMarkVisible)
+                                        GestureDetector(
+                                            child: Icon(
+                                                SFSymbols.xmark_circle_fill,
+                                                color: kXMarkCircleFontColor,
+                                            ),
+                                            onTap: () {
+                                                inputController.clear();
+                                            }
+                                        ),
+          
+                                ]),
+                        ),
+                    ),
+                    Flexible(flex: 1,child: Container()),
+                    if (focusNode.hasFocus)
+                        Expanded(
+                            flex: 2,
+                            child: Container(
+
+                                child: GestureDetector(
+                                    onTapDown: (TapDownDetails details) {
+                                        print(constraints);
+                                        onTapDownCancelBtn();
+                                    },
+                                    onTapUp: (TapUpDetails details) {
+                                        onTapUpCancelBtn();
+                                    },
+                                    onTapCancel: () {
+                                        onTapUpCancelBtn();
+                                    },
+                                    child: Container(
+                                        child: Text("Hủy",
+                                            textAlign: TextAlign.right,
+                                            style: TextStyle(color: cancelButtonFontColor,
+                                                overflow: TextOverflow.clip
+                                                )
+                                        )
+                                    ),
+                                ),
+                            ),
+                        )
+                ],
+            );
+        }));
+    }
+}
---
+++ lib/components/SearchBar/SearchBarConstant.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const double kSearchBarHeightRatio = 0.1;
+const double kSearchBarWidthRatio = 0.9;
+const double kSearchBarMaxHeight = 40;
+const double kSearchBarMaxWidth = double.infinity; 
+const double kSearchBarMinWidth = 300;
+const double kSearchBarMinHeight = 30;
+
+const double kSearchBarBorderRadius = 5.0;
+const Color kBackgroundSearchBarColor = Color.fromRGBO(238, 238, 238, 1);
+const Color kPlaceholderColor = Color.fromRGBO(185, 185, 188, 1);
+const double kPaddingBtwIconAndSearchBar = 10;
+const double kPaddingBtwSearchBarAndCancelBtn = 5;
+const Color kCancelButtonFontColor = Colors.red;
+
+const Color kXMarkCircleFontColor = Color.fromRGBO(144, 144, 148, 1);
